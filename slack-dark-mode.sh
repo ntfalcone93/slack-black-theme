3e573bf1bae93ca6c860669a4cd9270e5a4c4f41
Nathan T. Falcone <nfalcone@sofi.org>
2019-09-03 13:51:42 -0700

    Fixed word spacing

--- slack-dark-mode.sh
+++ slack-dark-mode.sh
@@ -4,7 +4,7 @@ JS="
 // First make sure the wrapper app is loaded
 document.addEventListener('DOMContentLoaded', function() {
   // Fetch our CSS in parallel ahead of time
-  const cssPath = 'https://raw.githubusercontent.com/caiceA/slack-raw/master/slack-4';
+  const cssPath = 'https://raw.githubusercontent.com/ntfalcone93/slack-black-theme/master/slack-dark-mode.css';
   let cssPromise = fetch(cssPath).then((response) => response.text());
 
   // Insert a style tag into the wrapper view
